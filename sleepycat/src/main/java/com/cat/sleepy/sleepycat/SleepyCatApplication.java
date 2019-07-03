5655290cdc657e5f5157be4691f3abc1b866ec6f
johosin <johosin@10.32.14.78>
2019-07-03 11:05:07 +0900

    commit and push

---
+++ sleepycat/src/main/java/com/cat/sleepy/sleepycat/SleepyCatApplication.java
@@ -0,0 +1,13 @@
+package com.cat.sleepy.sleepycat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepyCatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleepyCatApplication.class, args);
+	}
+
+}
