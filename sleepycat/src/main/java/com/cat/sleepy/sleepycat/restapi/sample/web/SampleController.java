25eeb63dfe30db54cd3adfb33cef5522ef252bb1
johosin <johosin@218.38.137.28>
2019-07-19 15:43:59 +0900

    RestController로 변

--- sleepycat/src/main/java/com/cat/sleepy/sleepycat/restapi/sample/web/SampleController.java
+++ sleepycat/src/main/java/com/cat/sleepy/sleepycat/restapi/sample/web/SampleController.java
@@ -4,28 +4,25 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.cat.sleepy.sleepycat.restapi.sample.dto.SampleDto;
 import com.cat.sleepy.sleepycat.restapi.sample.svc.SampleService;
 
-@Controller
+@RestController
 public class SampleController {
 
 	@Autowired
 	SampleService sampleService;
 	
 	@RequestMapping(value="/sleepycat", method=RequestMethod.GET)
-	@ResponseBody
 	public List<SampleDto> searchSample() {
 		return sampleService.searchUsers();
 	}
 	
 	@RequestMapping(value="/sleepycat/1", method=RequestMethod.GET)
-	@ResponseBody
 	public List<Map<String, Object>> searchSample1() {
 		return sampleService.searchUsers1();
 	}
